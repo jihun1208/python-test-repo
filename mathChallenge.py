0ec8e8e07ecd2d4cbaf2086ddbcdb52c05abebbb
jihun <teachingmeaning1208@gmail.com>
2025-11-06 21:49:22 +0900

    25/11/06

---
+++ mathChallenge.py
@@ -0,0 +1,32 @@
+import random
+import time
+
+OPERATORS = ["+", "-", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
+
+def generate_problem():
+    a = random.randint(MIN_OPERAND, MAX_OPERAND)
+    b = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+    expr = f"{a} {operator} {b}"
+    answer = eval(expr)
+    return expr, answer
+
+input("Press enter to start!")
+print("----------------------")
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i+1) + ": " + expr + " = ")
+        if guess == str(answer):
+            break
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print("----------------------")
+print(f"Nice work! You finished in {total_time} seconds!")
